5969ecfe6d31640dde5ffe612e4ce369fbcb3049
Daniel Martinez <daniel.martinez@talosdigital.com>
2018-06-06 14:41:08 -0500

    Adding requested changes

--- lesson-2/src/main/java/com/talos/javatraining/lesson2/Main.java
+++ lesson-2/src/main/java/com/talos/javatraining/lesson2/Main.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.Writer;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 import static org.apache.commons.lang3.StringUtils.CR;
@@ -31,21 +32,15 @@ public class Main
 			stations[i] = new WeatherStationImpl(i + 1);
 		}
 
-		// TODO change this annonymous inner class expression to a lambda expression
-		printer = new Printer<String>()
-		{
-			@Override
-			public void print(Measure<String> measure, Writer writer) throws IOException
-			{
+		printer = (measure, writer) ->  {
 				writer.write("---" + LF + CR);
 				writer.write("Station " + measure.getStationId() + LF + CR + LF + CR);
-				// TODO change these lambdas to method reference
-				printValue(() -> measure.getHumidity(), "Humidity", writer);
-				printValue(() -> measure.getWindSpeed(), "Wind speed", writer);
-				printValue(() -> measure.getPressure(), "Pressure", writer);
-				printValue(() -> measure.getTemperature(), "Temperature", writer);
-			}
+				printValue(measure::getHumidity, "Humidity", writer);
+				printValue(measure::getWindSpeed, "Wind speed", writer);
+				printValue(measure::getPressure, "Pressure", writer);
+				printValue(measure::getTemperature, "Temperature", writer);
 		};
+
 	}
 
 	public void readStations(Writer writer) throws IOException
@@ -74,6 +69,10 @@ public class Main
 		writer.write(label + " : " + getter.get() + LF + CR);
 	}
 
+	protected void setMeasureValue(Supplier<BigDecimal> getter, Consumer<String> setter, String unit) {
+		setter.accept(makeItReadable(getter.get(), unit));
+	}
+
 	protected Measure<String> makeItReadable(Measure<BigDecimal> measure)
 	{
 		Measure<String> result = new EasyToReadMeasure();
@@ -82,12 +81,18 @@ public class Main
 		/* TODO Refactor the following code.
 		 * Implement a new method that using method references gets and sets the humidity, windSpeed, pressure and temperature properties
 		 * You would call the makeItReadable method from that method
-		 * Eg: myMethod(measure::getHumidity, result::setHumidity, "unit")
-		 */
+		 * myMethod(measure::getPressure, result::getPressure, "unit")
+		*/
+		setMeasureValue(measure::getWindSpeed, result::setWindSpeed, "mph");
+		setMeasureValue(measure::getTemperature, result::setTemperature, "°C");
+		setMeasureValue(measure::getPressure, result::setPressure, "pa");
+		setMeasureValue(measure::getHumidity, result::setHumidity, "%");
+		/*
 		result.setWindSpeed(makeItReadable(measure.getWindSpeed(), "mph"));
 		result.setTemperature(makeItReadable(measure.getTemperature(), "°C"));
 		result.setPressure(makeItReadable(measure.getPressure(), "pa"));
 		result.setHumidity(makeItReadable(measure.getHumidity(), "%"));
+		*/
 		return result;
 	}
 
